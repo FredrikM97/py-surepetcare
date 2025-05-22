24628eaef5aba2477e75a59c96a93ef135085892
FredrikM97 <fredrik9779@gmail.com>
2025-05-22 10:59:23 +0200

    fix: Improve setup.sh to verify if local venv exist

--- setup.sh
+++ setup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-python3 -m venv .venv
+set -e 
+
+if [ -d ".venv" ]; then
+  echo "Using existing virtual environment."
+else
+  python3 -m venv .venv
+fi
+
 source .venv/bin/activate
-pip install -r requirements.txt
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
 echo "Don't forget to create your .env file!"
