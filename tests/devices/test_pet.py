d7a8bb89d92f06aaa57b75891aac00503322ae4d
Fredrik Mårtensson <fredrik9779@gmail.com>
2025-09-08 16:59:25 +0200

    Add type hint to snapshot argument (#68)

--- tests/devices/test_pet.py
+++ tests/devices/test_pet.py
@@ -1,6 +1,7 @@
 import json
 
 import pytest
+from syrupy.assertion import SnapshotAssertion
 
 from surepcio.household import Household
 from tests.mock_helpers import MockClient
@@ -18,8 +19,8 @@ def household_file():
 
 
 @pytest.mark.asyncio
-async def test_snapshot(snapshot, household_file, device_file):
-    # snapshot.snapshot_dir = "tests/snapshots"
+async def test_snapshot(snapshot: SnapshotAssertion, household_file, device_file):
+    snapshot.snapshot_dir = "tests/snapshots"
     client = MockClient(fixture_file=device_file)
     client.set_mock_response(household_file)
     household = await client.api(Household.get_household(7777))
