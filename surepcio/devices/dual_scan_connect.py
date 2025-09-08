52e4fb8b39519575b2e061a58168b148dc9515e2
FredrikM97 <fredrik9779@gmail.com>
2025-09-08 16:43:45 +0200

    Add dual_scan_connect device data

--- surepcio/devices/dual_scan_connect.py
+++ surepcio/devices/dual_scan_connect.py
@@ -1,4 +1,7 @@
 import logging
+from typing import Optional
+
+from surepcio.devices.entities import FlattenWrappersMixin
 
 from .device import BaseControl
 from .device import BaseStatus
@@ -6,16 +9,37 @@ from .device import DeviceBase
 from surepcio.command import Command
 from surepcio.const import API_ENDPOINT_PRODUCTION
 from surepcio.enums import ProductId
+from datetime import time
 
 logger = logging.getLogger(__name__)
+
+class Curfew(FlattenWrappersMixin):
+    enabled: bool
+    lock_time: time
+    unlock_time: time
+    
+class Control(BaseControl):
+    curfew: Optional[list[Curfew]] = None
+    locking: Optional[int] = None
+    fail_safe: Optional[int] = None
+    fast_polling: Optional[bool] = None
+
+
+class Locking(FlattenWrappersMixin):
+    mode: int = 0
+    
+class Status(BaseStatus):
+    locking: Optional[Locking] = None
+
+
 
 
 class DualScanConnect(DeviceBase):
     def __init__(self, data: dict) -> None:
         try:
             super().__init__(data)
-            self.status: BaseStatus = BaseStatus(**data)
-            self.control: BaseControl = BaseControl(**data)
+            self.status: Status = Status(**data)
+            self.control: Control = Control(**data)
         except Exception as e:
             logger.warning("Error while storing data %s", data)
             raise e
@@ -28,8 +52,8 @@ class DualScanConnect(DeviceBase):
         def parse(response):
             if not response:
                 return self
-            self.status = BaseStatus(**{**self.status.model_dump(), **response["data"]})
-            self.control = BaseControl(**{**self.control.model_dump(), **response["data"]})
+            self.status = Status(**{**self.status.model_dump(), **response["data"]})
+            self.control = Control(**{**self.control.model_dump(), **response["data"]})
             return self
 
         return Command(
