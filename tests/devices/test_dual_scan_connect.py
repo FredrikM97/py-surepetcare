d41bb93023ca1147685e721f449852bee1b27f66
Fredrik Mårtensson <fredrik9779@gmail.com>
2025-09-08 17:16:52 +0200

    Add device DualScan Connect (#65)

---
+++ tests/devices/test_dual_scan_connect.py
@@ -0,0 +1,28 @@
+import pytest
+from syrupy.assertion import SnapshotAssertion
+
+from surepcio.household import Household
+from tests.mock_helpers import MockClient
+from tests.mock_helpers import recursive_dump
+
+
+@pytest.fixture
+def device_file():
+    return "tests/fixture/dual_scan_connect.json"
+
+
+@pytest.fixture
+def household_file():
+    return "tests/fixture/household.json"
+
+
+@pytest.mark.asyncio
+async def test_snapshot(snapshot: SnapshotAssertion, household_file, device_file):
+    snapshot.snapshot_dir = "tests/snapshots"
+    client = MockClient(fixture_file=device_file)
+    client.set_mock_response(household_file)
+    household = await client.api(Household.get_household(7777))
+    client.reset()
+    device = (await client.api(household.get_devices()))[0]
+    await client.api(device.refresh())
+    assert recursive_dump(device) == snapshot
