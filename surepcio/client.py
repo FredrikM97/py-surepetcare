929390795e77f99f74af4fddc69707a05a6fbc17
FredrikM97 <fredrik9779@gmail.com>
2025-09-12 09:21:49 +0000

    Added put/delete to client and missing data issue in dataclass

--- surepcio/client.py
+++ surepcio/client.py
@@ -11,13 +11,13 @@ class SurePetcareClient(AuthClient):
         await self.set_session()
         async with self.session.get(endpoint, params=params, headers=headers) as response:
             if not response.ok:
-                raise Exception("Error %s %s: %s", endpoint, response.status, await response.text())
+                raise Exception(f"Error {endpoint} {response.status}: {await response.text()}")
             if response.status == 204:
-                logger.info("GET %s returned 204 No Content", endpoint)
+                logger.info(f"GET {endpoint} returned 204 No Content")
                 return None
             if response.status == 304:
                 # Not modified, keep existing data
-                logger.debug("GET %s returned 304 Not Modified", endpoint)
+                logger.info(f"GET {endpoint} returned 304 Not Modified")
                 return None
             self.populate_headers(response)
             return await response.json()
@@ -26,11 +26,39 @@ class SurePetcareClient(AuthClient):
         await self.set_session()
         async with self.session.post(endpoint, json=data, headers=headers) as response:
             if not response.ok:
-                raise Exception("Error %s: %s", response.status, await response.text())
+                raise Exception(f"Error {response.status}: {await response.text()}")
             if response.status == 204:
-                logger.info("POST %s returned 204 No Content", endpoint)
-                return {"status": 204}
+                logger.info(f"POST {endpoint} returned 204 No Content")
+                return {"status": response.status}
             self.populate_headers(response)
+            return await response.json()
+
+    async def put(self, endpoint: str, data: dict | None = None, headers=None, reuse=True) -> dict:
+        await self.set_session()
+        async with self.session.put(endpoint, json=data, headers=headers) as response:
+            if not response.ok:
+                raise Exception(f"Error {response.status}: {await response.text()}")
+            if response.status == 204:
+                logger.info("PUT {endpoint} returned 204 No Content")
+                return {"status": response.status}
+            logger.info(f"DELETE {endpoint} returned {response.status}")
+            self.populate_headers(response)
+            return await response.json()
+
+    async def delete(self, endpoint: str, data: dict | None = None, headers=None, reuse=True) -> dict:
+        await self.set_session()
+        async with self.session.delete(endpoint, json=data, headers=headers) as response:
+            if not response.ok:
+                raise Exception(f"Error {response.status}: {await response.text()}")
+            if response.status == 204:
+                logger.info(f"DELETE {endpoint} returned 204 No Content")
+                return {"status": response.status}
+            logger.info(f"DELETE {endpoint} returned {response.status}")
+            self.populate_headers(response)
+            # Delete even if 200 sometimes has no content
+            if response.content_length == 0:
+                return {"status": response.status}
+
             return await response.json()
 
     async def api(self, command: Command):
@@ -48,7 +76,19 @@ class SurePetcareClient(AuthClient):
                 data=command.params,
                 headers=headers,
             )
+        elif method == "put":
+            coro = self.put(
+                command.endpoint,
+                data=command.params,
+                headers=headers,
+            )
 
+        elif method == "delete":
+            coro = self.delete(
+                command.endpoint,
+                data=command.params,
+                headers=headers,
+            )
         else:
             raise NotImplementedError("HTTP method %s not supported.", command.method)
         response = await coro
