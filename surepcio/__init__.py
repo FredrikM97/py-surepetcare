1f07d55d0d48512a1eaa98bc5ad3f7bbca3b6dd7
FredrikM97 <fredrik9779@gmail.com>
2025-09-17 20:21:30 +0200

    Import Household and SurePetCareClient in __init__

--- surepcio/__init__.py
+++ surepcio/__init__.py
@@ -1,8 +1,11 @@
 import logging
 
 from surepcio.security.redact import RedactSensitiveFilter
+from surepcio.client import SurePetcareClient
+from surepcio.household import Household
 
 handler = logging.StreamHandler()
 handler.addFilter(RedactSensitiveFilter())
 
 logging.basicConfig(level=logging.INFO, handlers=[handler])
+
