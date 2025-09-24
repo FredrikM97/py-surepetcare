d6f7855558bf07a870a53d0d7149a7d58761f575
FredrikM97 <fredrik9779@gmail.com>
2025-09-24 16:43:56 +0000

    Iterate all loggers starting with surepcio and add filter

--- surepcio/__init__.py
+++ surepcio/__init__.py
@@ -4,8 +4,8 @@ from surepcio.client import SurePetcareClient  # noqa: F401
 from surepcio.household import Household  # noqa: F401
 from surepcio.security.redact import RedactSensitiveFilter
 
-logger = logging.getLogger("surepcio")
-handler = logging.StreamHandler()
-handler.addFilter(RedactSensitiveFilter())
-logger.addHandler(handler)
-logger.propagate = True
+f = RedactSensitiveFilter()
+
+for name in logging.Logger.manager.loggerDict:
+    if name.startswith("surepcio"):
+        logging.getLogger(name).addFilter(f)
