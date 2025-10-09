13bb689722385531673c9e95842f58db600e3acf
Fredrik Mårtensson <fredrik9779@gmail.com>
2025-10-09 09:16:49 +0000

    Remove custom error check to simplify workflow

--- surepcio/security/exceptions.py
+++ surepcio/security/exceptions.py
@@ -1,8 +1,2 @@
 class AuthenticationError(Exception):
     pass
-
-
-class ValidationMissingFieldsError(Exception):
-    """Raised when required fields are missing during model validation."""
-
-    pass
